805792b75a29743e3051b1283f0f72d3587d69f5
SumTush <samkilonzi@.com>
2023-03-17 11:30:24 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print a string
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
