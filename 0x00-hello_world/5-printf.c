c46aaee80782931ef70d78170c0f4201d9c42ee2
SumTush <samkilonzi@.com>
2023-03-17 12:23:07 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print a string
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
