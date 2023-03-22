a1697b60ea2920d0dee37cf1c6e9f2984f5b949c
SumTush <samkilonzi@.com>
2023-03-22 11:10:05 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
-int_putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int print_sign(int n);
+int _putchar(char c);
+void _print_alphabet(void);
+void _print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
