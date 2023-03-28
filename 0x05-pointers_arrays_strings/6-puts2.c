bd655b6b925940f69458b6f22e15553921e734b9
SumTush <samkilonzi@.com>
2023-03-28 12:48:28 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints "with proper grammar, but the outcome is
- * a piece of art," followed neewline
- * Return: Always 0.
- *
+ * puts2 - Prints every other character of a string
+ * @str: The string to be treated
+ * Return: void
  */
 
-
-int main(void)
+void puts2(char *str)
 
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
-	return (0);
+
+	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
 }
